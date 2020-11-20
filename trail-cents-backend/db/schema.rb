cff47deddceaeeccea14071b5dd4170156e743fe
Jackson Reynolds <jwreynold@gmail.com>
2020-11-20 14:17:26 -0500

    added email to users, built user serializer

--- trail-cents-backend/db/schema.rb
+++ trail-cents-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_175555) do
+ActiveRecord::Schema.define(version: 2020_11_20_191401) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_11_20_175555) do
     t.decimal "points"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end

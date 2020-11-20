ed145a25207c40d4ffc887fcb8a00f6dda6690bd
Jackson Reynolds <jwreynold@gmail.com>
2020-11-20 12:57:02 -0500

    created db
    users and events tables migrated using resource generator, so routes and models etc crated as well

---
+++ trail-cents-backend/db/migrate/20201120175555_create_events.rb
@@ -0,0 +1,17 @@
+class CreateEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.decimal :lat
+      t.decimal :long
+      t.text :desc
+      t.date :date
+      t.string :category
+      t.time :start
+      t.time :end
+      t.decimal :duration
+
+      t.timestamps
+    end
+  end
+end

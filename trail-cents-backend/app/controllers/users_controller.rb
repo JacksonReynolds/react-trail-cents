20127fb52b5f1c38e3fa750b6b7db377b60cc1a7
Jackson Reynolds <jwreynold@gmail.com>
2020-11-20 14:07:58 -0500

    user controller action buildout

--- trail-cents-backend/app/controllers/users_controller.rb
+++ trail-cents-backend/app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
-end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: UserSerializer.new(user)
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+end 

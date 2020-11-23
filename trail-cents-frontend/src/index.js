f425f84631ff2bb2f04e0c1e845a2b537d8b9fae
Jackson Reynolds <jwreynold@gmail.com>
2020-11-23 11:33:57 -0500

    building top components

--- trail-cents-frontend/src/index.js
+++ trail-cents-frontend/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom'
+import {Provider} from 'react-redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+// import reducers
 import App from './App';
 
 ReactDOM.render(

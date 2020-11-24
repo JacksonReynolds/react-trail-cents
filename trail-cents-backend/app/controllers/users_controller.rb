82ee269de1c54c1233bd7ffd791b4c320e0755db
Jackson Reynolds <jwreynold@gmail.com>
2020-11-24 14:47:54 -0500

    added errors

--- trail-cents-backend/app/controllers/users_controller.rb
+++ trail-cents-backend/app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
         user = User.new(user_params)
         if user.save
             render json: UserSerializer.new(user)
+        else
+            render json: {errors: user.errors.full_messages}
         end
     end
 

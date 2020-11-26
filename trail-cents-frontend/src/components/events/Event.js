de881a7189ed8b4e28e4112508120170a23a2b5c
Jackson Reynolds <jwreynold@gmail.com>
2020-11-25 21:18:09 -0500

    imgs showing

--- trail-cents-frontend/src/components/events/Event.js
+++ trail-cents-frontend/src/components/events/Event.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
-import tf from '../../../public/imgs/trailforks_avatar.png'
+import tf from '../../imgs/trailforks_avatar.png'
 
 const Event = (props) => {
     return (
         <>
             <Card>
-                <Card.Img variant="top" src="../../ public/imgs/trailforks_avatar.png" alt="nope"/>
+                <Card.Img variant="top" src={tf} alt="nope"/>
                 <Card.Body>
                     <Card.Title>{props.event.title}</Card.Title>
                     <Card.Text>

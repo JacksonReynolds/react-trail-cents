61f961348bbc089f4bef28edec5d259a27e9a4c5
Jackson Reynolds <jwreynold@gmail.com>
2020-11-27 23:16:28 -0500

    switch to let UserSerializer take over

--- trail-cents-backend/app/controllers/users_controller.rb
+++ trail-cents-backend/app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            render json: {user: user}
+            render json: user
         else
             render json: {errors: user.errors.full_messages}
         end

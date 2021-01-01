f23b4b11b2ea834ce39fffff2baabf9199ed43a4
Jackson Reynolds <jwreynold@gmail.com>
2021-01-01 11:35:22 -0500

    included token tests

--- trail-cents-backend/spec/requests/users_spec.rb
+++ trail-cents-backend/spec/requests/users_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  before do
+    @header = {'accepts': 'application/json'}
+  end
+
   describe "POST /users" do
-    before do
-      @header = {'accepts': 'application/json'}
-    end
     context "w/ valid params" do
       before do
         valid_params = {user: {username: 'valid user', 
@@ -13,6 +14,7 @@ RSpec.describe "Users", type: :request do
                                email: 'email@email.com'}}
         post '/users', :params => valid_params, :headers => @header
         @user = User.find_by(id: JSON.parse(response.body)['user']['id'])
+        @token_body = JWT.decode(JSON.parse(response.body)['jwt'], ENV['secret'], true, algorithm: 'HS256')[0]
       end
 
       it 'returns successful' do
@@ -21,6 +23,20 @@ RSpec.describe "Users", type: :request do
 
       it 'creates a user record' do 
         expect(@user).to be_an_instance_of(User)
+      end
+
+      it 'includes a token in the reponse' do
+        expect(@token_body).to be
+      end
+
+      # test contents of token
+      it 'w/ user id' do
+        expect(@token_body['user_id']).to equal(@user.id)
+      end
+
+      it 'w/ an expiration' do
+        time = Time.at(@token_body['exp'])
+        expect(@token_body['exp'])
       end
     end
 
@@ -39,6 +55,14 @@ RSpec.describe "Users", type: :request do
 
       it 'returns errors' do
         expect(response.body).to include("Username can't be blank", "Email can't be blank", "Password confirmation doesn't match Password")
+      end
+    end
+  end
+
+  describe "PATCH /users/:id" do
+    context "w/ valid params" do
+      before do
+
       end
     end
   end

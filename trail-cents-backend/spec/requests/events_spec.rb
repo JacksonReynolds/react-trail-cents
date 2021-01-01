20c2f78cee9e88cb426dbe9328f9338eeb8be9ed
Jackson Reynolds <jwreynold@gmail.com>
2021-01-01 14:13:23 -0500

    events spec

--- trail-cents-backend/spec/requests/events_spec.rb
+++ trail-cents-backend/spec/requests/events_spec.rb
@@ -20,10 +20,19 @@ RSpec.describe "Events", type: :request do
       {title: 'crazy trail day', duration: 4.0},
       {title: 'crazy trail day', duration: 4.0}
     ])
+    @events = Event.all
   end
   describe "GET /events" do
     before do
+      get '/events'
+    end
 
+    it 'returns successful' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns an array of event records' do
+      expect(JSON.parse(response.body).length).to eq(@events.length)
     end
   end
 end

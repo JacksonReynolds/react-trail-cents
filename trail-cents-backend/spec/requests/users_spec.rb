32e825e86828cbd9c26b8449b33fc804ce083862
Jackson Reynolds <jwreynold@gmail.com>
2021-01-01 11:04:38 -0500

    invalid user request tests

--- trail-cents-backend/spec/requests/users_spec.rb
+++ trail-cents-backend/spec/requests/users_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "POST /users" do
+    before do
+      @header = {'accepts': 'application/json'}
+    end
     context "w/ valid params" do
       before do
-        header = {'accepts': 'application/json'}
         valid_params = {user: {username: 'valid user', 
                                password: 'password',
                                password_confirmation: 'password',
                                email: 'email@email.com'}}
-        post '/users', :params => valid_params, :headers => header
+        post '/users', :params => valid_params, :headers => @header
         @user = User.find_by(id: JSON.parse(response.body)['user']['id'])
       end
 
@@ -19,6 +21,24 @@ RSpec.describe "Users", type: :request do
 
       it 'creates a user record' do 
         expect(@user).to be_an_instance_of(User)
+      end
+    end
+
+    context "w/ invalid params" do
+      before do
+        invalid_params = {user: {username: '',
+                                 password: 'something',
+                                 password_confirmation: 'something_else',
+                                 email: ''}}
+        post '/users', :params => invalid_params, :headers => @header
+      end
+
+      it 'returns unsuccessful' do
+        expect(response).to have_http_status(:error)
+      end
+
+      it 'returns errors' do
+        expect(response.body).to include("Username can't be blank", "Email can't be blank", "Password confirmation doesn't match Password")
       end
     end
   end

d57e78fd27afe3fc8a3380ecad8ecce7df56521d
Jackson Reynolds <jwreynold@gmail.com>
2021-01-01 16:26:32 -0500

    working on token response tests

--- trail-cents-backend/spec/requests/auths_spec.rb
+++ trail-cents-backend/spec/requests/auths_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe "Auth", type: :request do
       before do
         @user = User.create(username: 'user', password: 'pw', password_confirmation: 'pw', email: 'me@email.com')
         payload = {user_id: @user.id, exp: Time.now.to_i+3600}
-        @token = JWT.encode(payload, ENV['secret'])
-        headers = {'accepts': 'application/json', "Authorization": "Bearer #{@token}"}
+        token = JWT.encode(payload, ENV['secret'])
+        headers = {'accepts': 'application/json', "Authorization": "Bearer #{token}"}
 
         post '/auth', :headers => headers
       end
@@ -20,6 +20,57 @@ RSpec.describe "Auth", type: :request do
         user_data = JSON.parse(response.body)
         expect(user_data).to be
         expect(user_data["id"]).to eq(@user.id)
+      end
+    end
+
+    context 'w/ invalid token' do
+      before do
+        headers = {'accepts': 'application/json', "Authorization": "Bearer FakeToken"}
+
+        post '/auth', :headers => headers
+      end
+
+      it 'returns unsuccessful' do
+        expect(response).to have_http_status(:error)
+      end
+
+      it 'returns errors' do
+        expect(response.body).to include("Invalid token, try logging in with email and password")
+      end
+    end
+
+    context 'w/ expired token' do
+      before do
+        @user = User.create(username: 'validuser', password: 'pw', password_confirmation: 'pw', email: 'notme@email.com')
+        payload = {user_id: @user.id, exp: Time.now.to_i-1}
+        token = JWT.encode(payload, ENV['secret'])
+        headers = {'accepts': 'application/json', "Authorization": "Bearer #{token}"}
+
+        post '/auth', :headers => headers
+      end
+
+      it 'returns unsuccessful' do
+        expect(response).to have_http_status(:error)
+      end
+
+      it 'returns errors' do
+        expect(response.body).to include("Expired session, please log back in")
+      end
+    end
+
+    context 'w/ invalid login credentials' do
+      before do
+        invalid_user_params = {user: {email: 'not@valid.com', password: 'nope'}}
+
+        post '/auth', :params => invalid_user_params
+      end
+
+      it 'returns unsuccessful' do
+        expect(response).to have_http_status(:error)
+      end
+
+      it 'returns errors' do
+        expect(response.body).to include("Incorrect email/password combo")
       end
     end
   end

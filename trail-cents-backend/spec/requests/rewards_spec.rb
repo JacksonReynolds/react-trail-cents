61e188a09295358c436cdb8cfd38e04c080113cd
Jackson Reynolds <jwreynold@gmail.com>
2021-01-01 14:05:29 -0500

    reward patch tests

--- trail-cents-backend/spec/requests/rewards_spec.rb
+++ trail-cents-backend/spec/requests/rewards_spec.rb
@@ -27,12 +27,31 @@ RSpec.describe "Rewards", type: :request do
     end
 
     it 'returns an array of reward records' do
-      binding.pry
       expect(JSON.parse(response.body).length).to eq(@rewards.length)
     end
   end
 
   describe 'PATCH /rewards/:id' do
-    
+    context 'w/ valid params' do
+      before do
+        patch "/rewards/1"
+      end
+
+      it 'returns successful' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'reduces the number available of that reward' do
+        reward = Reward.find_by(id: 1)
+        expect(reward.num_available).to eq(4)
+      end
+
+      it 'returns updated reward data' do
+        reward_data = JSON.parse(response.body)
+        expect(reward_data).to be
+        expect(reward_data['id']).to eq(1)
+        expect(reward_data['num_available']).to eq(4)
+      end
+    end
   end
 end

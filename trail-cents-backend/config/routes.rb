8cd30a27b02af3c642868adf9aae62021543ce87
Jackson Reynolds <jwreynold@gmail.com>
2021-01-01 14:22:18 -0500

    limit resource controller actions

--- trail-cents-backend/config/routes.rb
+++ trail-cents-backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :rewards
-  resources :events
-  resources :users
-  resources :auth
+  resources :rewards, :only => [:index, :update]
+  resources :events, :only => [:index]
+  resources :users, :only => [:create, :update]
+  resources :auth, :only => [:create]
 end
